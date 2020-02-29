f4aa6c9357f9b39936beecabf447f118ee2878d8
DamianSkupski <d.skupski@wp.pl>
2020-02-29 15:47:03 +0100

    zad domowe 1 zalozenia

--- .idea/sda/tabela_book.sql
+++ .idea/sda/tabela_book.sql
@@ -47,6 +47,11 @@ USE library;
 INSERT INTO book (title, author, published, isbn, category, page_count, publisher, price)
 VALUES ('Spring w akcji Wydanie IV', 'Craig Walls', '2015-08-13', '978-83-283-0849-7', 'Programowanie java', 624, 'Helion', 89.00);
 
+UPDATE book
+SET title = 'Spring w akcji. Wydanie IV'
+WHERE isbn = '978-83-283-0849-7';
+
+
 INSERT INTO book(title, author, published, isbn, category, page_count, publisher, price)
 VALUES ('MySQL. Vademecum profesjonalisty', 'Paul DuBois', '2014-03-28', '978-83-246-8146-4', 'Bazy danych', 1216, 'Helion', 149.00);
 
@@ -61,9 +66,26 @@ ALTER TABLE book MODIFY COLUMN publisher VARCHAR(128) DEFAULT 'nieznana';
 ALTER TABLE book ADD COLUMN in_stock INT DEFAULT 0;
 
 DESC book;
+SELECT * FROM book;
 
+SELECT * FROM book WHERE price BETWEEN 50 AND 90;
+SELECT * FROM book WHERE isbn IN ('978-83-283-0849-7', '978-83-246-8146-4');
+SELECT * FROM book WHERE title LIKE ('Spring w akcji. Wydanie I_');
+SELECT * FROM book WHERE title LIKE ('%Spring%');
 
+SELECT DISTINCT publisher FROM book;
 
+SELECT publisher, SUM(price)
+FROM book
+GROUP BY publisher;
+
+SELECT SUM(page_count) AS total_pages FROM book;
+SELECT AVG(page_count) FROM book;
+SELECT MIN(page_count) FROM book;
+SELECT MAX(page_count) FROM book;
+
+SELECT * FROM book
+WHERE EXISTS(SELECT * FROM book WHERE price < 100);
 
  
 
